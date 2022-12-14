1290ed3f67b245b6c2ac5dd1483d330b0eef21b8
arch-il <archilberaia+github@gmail.com>
2022-12-14 21:53:26 +0400

    Remove empty spaces in Program.cs

--- FootBall.API/Program.cs
+++ FootBall.API/Program.cs
@@ -3,9 +3,7 @@ using FootBall.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
