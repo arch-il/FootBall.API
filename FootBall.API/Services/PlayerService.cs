b6044aabe68155877dca08eab499bb29ade84cb4
arch-il <archilberaia+github@gmail.com>
2022-12-17 00:40:46 +0400

    Added Player and migrated it to database

---
+++ FootBall.API/Services/PlayerService.cs
@@ -0,0 +1,22 @@
+﻿namespace FootBall.API.Services
+{
+    using FootBall.API.Interfaces;
+    using FootBall.API.Models;
+    public class PlayerService : IPlayerService
+    {
+        public Player GetConcrettePlayer(Player player)
+        {
+            if (player == null ||
+                player.Name.Length < 3 ||
+                player.Surname.Length < 3 ||
+                player.Age < 14 ||
+                player.Platform.Length < 3 ||
+                player.Rating < 0
+                )
+            {
+                return null;
+            }
+            return player;
+        }
+    }
+}
